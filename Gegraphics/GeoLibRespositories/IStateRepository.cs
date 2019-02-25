e92ff3370fcaf97d038e67c3767f989bf9d9dd51
Pranshu Gupta <pranshugupta18@gmail.com>
2019-02-26 00:14:07 +0530

    WCF Geographic

--- Gegraphics/GeoLibRespositories/IStateRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace GeoLibRespositories
-{
-    public interface IStateRepository
-    {
-        IEnumerable<string> GetStates(bool primaryOnly);
-    }
-}
--- Gegraphics/GeoLibRespositories/ZipCode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeoLibRespositories
-{
-    public class ZipCode
-    {
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-    }
-}
